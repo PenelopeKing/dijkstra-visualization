185a6ba5128963a6ff44cb293de31d35991e7cfe
GarveyJLi <GARVEY.J.LI@GMAIL.COM>
2024-02-28 17:44:45 -0800

    generated data

--- src/components/Nodes.svelte
+++ src/components/Nodes.svelte
@@ -1,5 +1,41 @@
 <script>
+    import { nodes } from "../data/ex_nodes"
+    import { edges } from "../data/ex_edges"
+    import { fly, draw } from "svelte/transition";
+    import { tweened } from "svelte/motion";
+    import * as d3 from 'd3';
+
+    export let height, width, index;
+    let svg
+
     
+    console.log(nodes)
+    console.log(edges)
 
+    $: x=d3
+    .scaleUtc() /* date as input and axis as output */
+    .domain(d3.extent(nodes.nodes, (d) => d.x)) /* min and max vals */
+    .range([marginLeft, width - marginRight])
 
+    $: y=d3
+    .scaleLinear()
+    .domain(d3.extent(nodes.nodes, (d) => d.y))
+    .range([height - marginBottom, marginTop])
 </script>
+
+<div class="weighted-graph">
+    <svg
+    bind:this={svg}
+    {width}
+    {height}
+    viewBox="0 0 {width} {height}"
+    style="max-width: 100%; height: auto;"
+    >
+  <g stroke="#000" stroke-opacity="0.2">
+    {#each nodes.nodes as n}
+    <!--{} means fill with JS expression-->
+      <circle key={n.id} cx={x(n.x)} cy={y(n.y)} r="2.5"/>
+    {/each}
+    </g>
+
+</div>
---
+++ src/data/ex_edges.js
@@ -0,0 +1,40 @@
+export const edges = {
+    "edges": [
+    {
+      "source": 1,
+      "target": 6
+    },
+    {
+      "source": 1,
+      "target": 2
+    },
+    {
+      "source": 1,
+      "target": 3
+    },
+    {
+      "source": 2,
+      "target": 3
+    },
+    {
+      "source": 2,
+      "target": 3
+    },
+    {
+      "source": 6, 
+      "target": 2
+    },
+    {
+      "source": 3,
+      "target": 5
+    },
+    {
+      "source": 5,
+      "target": 4
+    },
+    {
+      "source": 6,
+      "target": 4
+    }
+  ]
+}
