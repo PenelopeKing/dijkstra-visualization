d0d96439852a21d0c4f1c0500f7b72d893d2750d
PenelopeKing <pby.king@gmail.com>
2024-03-01 15:10:40 -0800

    solutions added

---
+++ src/data/sol_edges.js
@@ -0,0 +1,24 @@
+export const edges = {
+    "edges": [
+    {
+      "source": 0,
+      "target": 1,
+      "color":'#5e5e5e'
+    },
+    {
+      "source": 1,
+      "target": 2,
+      "color":'#5e5e5e'
+    },
+    {
+      "source": 2,
+      "target": 4,
+      "color":'#5e5e5e'
+    },
+    {
+      "source": 4,
+      "target": 3,
+      "color":'#5e5e5e'
+    }
+  ]
+}
---
+++ src/data/sol_nodes.js
@@ -0,0 +1,34 @@
+export const nodes = {
+  "nodes": [
+    {
+      "id": 0,
+      "name": "A",
+      "x": 0,
+      "y": 0,
+      "color":"#7083ff"
+      
+    },
+    {
+      "id": 2,
+      "name": "C",
+      "x": 1,
+      "y": 3,
+      "color":"pink"
+    },
+    {
+      "id": 3,
+      "name": "D",
+      "x": 5,
+      "y": 0,
+      "color":"red"
+    },
+    {
+      "id": 4,
+      "name": "E",
+      "x": 4,
+      "y": 2,
+      "color":"pink"
+    }
+  ],
+  
+}
