11d74c0346ae552449149a0185f602d05d7cca76
PenelopeKing <pby.king@gmail.com>
2024-03-01 12:33:28 -0800

    got spacing and animations to kind of work

--- src/components/Nodes.svelte
+++ src/components/Nodes.svelte
@@ -1,5 +1,4 @@
 <script>
-
     // get node data with nodes.nodes
     import { nodes } from "../data/ex_nodes"
     // get edge data with edges.edges
@@ -7,37 +6,42 @@
     import { fly, draw , fade} from "svelte/transition";
     import { cubicOut, cubicInOut } from "svelte/easing";
     import { tweened } from "svelte/motion";
-
-    
     import * as d3 from 'd3';
-
     export let height, width, index;
     const marginLeft = 100;
-    const marginRight = 450;
-    const marginTop = 200;
-    const marginBottom = 200;
-
+    const marginRight = 100;
+    const marginTop = 100;
+    const marginBottom = 100;
     let currNode;
     let svg;
-
+    let tempx;
+    let tempy;
+    const blue = "#7083ff"
+    
+    let blueLines = [];
     $: x=d3
     .scaleLinear() /* date as input and axis as output */
     .domain(d3.extent(nodes.nodes, (d) => d.x)) /* min and max vals */
-    .range([marginLeft, width - marginRight])
-
+    .range([marginLeft * 1.25, width / 2])
     $: y=d3
     .scaleLinear()
     .domain(d3.extent(nodes.nodes, (d) => d.y))
-    .range([height - marginBottom, marginTop])
-
+    .range([height * 0.75, marginTop * 2])
     const tweenOptions = {
     delay: 0,
     duration: 1000,
     easing: cubicOut,
   };
-    let rectX = tweened(100, { duration: 1000, easing: cubicOut });
-    let rectY = tweened(360, { duration: 1000, easing: cubicOut });
-
+    let rectX = tweened(130, { duration: 1000, easing: cubicOut });
+    let rectY = tweened(380, { duration: 1000, easing: cubicOut });
+  function setRect() {
+    rectX = tweened(130, { duration: 1000, easing: cubicOut });
+    rectY = tweened(380, { duration: 1000, easing: cubicOut });
+  }
+  function resetUserInteraction() {
+    d3.select(svg).selectAll("*").remove();
+    blueLines = [];
+  }
     function setCurrNode(node){
         currNode = node;
         console.log('set currNode')
@@ -48,21 +52,13 @@
         // Update position of rectangle to follow edge to clicked node
         updateRectanglePosition(node);
     }
-    function hasMatchingId(dict1, dict2) {
-        if (dict1.id === dict2.id){
-            return dict1
-        }
-    }
+
     // Function to update position of rectangle based on selected node
     function updateRectanglePosition(selectedNode) {
         //console.log(selectedNode)
         const edges_lst = edges.edges.filter(e => (e.source === selectedNode.id || e.target === selectedNode.id));
         if (!edges_lst) return; // No edge found
 
-        //console.log(edges_lst)
-        //const sourceNode = nodes.nodes.find(n => n.id === edge_lst.source);
-        //const sourceNode = nodes.nodes.find(node => edges_lst.some(edge => edge.source === node.id));
-        //console.log(sourceNode)
         const targetNode = selectedNode; //nodes.nodes.find(n => n.id === edge.target);
         const exists = edges_lst.some(edge => edge.source === currNode.id); 
         if (!exists) {
@@ -70,22 +66,62 @@
             return;
         }
         const sourceNode = currNode;
-
         const connected_edge = edges.edges.find(e => (e.source == sourceNode.ig && e.target == selectedNode.id))
+        
         // animate movement of car
-        rectX.set(x(sourceNode.x) + (x(targetNode.x) - x(sourceNode.x)) - 5);
+        rectX.set(x(sourceNode.x) + (x(targetNode.x) - x(sourceNode.x)) + 25);
         rectY.set(y(sourceNode.y) + (y(targetNode.y) - y(sourceNode.y)) - 7);
+        
+        // draw new lines
+        tempx = x(sourceNode.x) + (x(targetNode.x) - x(sourceNode.x)) + 20;
+        tempy = y(sourceNode.y) + (y(targetNode.y) - y(sourceNode.y)) - 7;
+        // Calculate the new points for the polyline
+
+
+        const points = `${x(currNode.x)},${y(currNode.y)} ${tempx - 20 },${tempy +7} ${x(selectedNode.x)},${y(selectedNode.y)}`;
+
+        // Update the polyline's points
 
         // update edge and node colors
+        // Create a new polyline element for the blue line
+        const newLine = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+        newLine.setAttribute("points", points);
+        newLine.setAttribute("stroke", blue);
+        newLine.setAttribute("stroke-width", "7");
+        newLine.setAttribute("fill", "none");
+        newLine.style.opacity = 0;
+        // Add the new line to the SVG
+        svg.appendChild(newLine);
+        // transition
+        setTimeout(() => {
+            newLine.style.opacity = 1;
+        }, 500);
+        
+        blueLines.push(newLine);
 
+        // add new circle
+        const newCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        newCircle.setAttribute("fill" , blue);
+        newCircle.setAttribute("r", '20');
+        newCircle.setAttribute('cx' , `${x(targetNode.x)}`);
+        newCircle.setAttribute('cy' , `${y(targetNode.y)}`);
+        newCircle.style.opacity = 0;
+        svg.appendChild(newCircle);
+        blueLines.push(newCircle)
+        setTimeout(() => {
+            newCircle.style.opacity = 1;
+        }, 500);
+        // cx={x(n.x)} 
+        // cy={y(n.y)} 
 
+        // create new circle
         setCurrNode(targetNode); // change what currNode is 
     }
 
-    
 </script>
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
+
 <div class="weighted-graph">
     <svg class="graph"
     bind:this={svg}
@@ -94,7 +130,13 @@
     viewBox="0 0 {width} {height}"
     style="max-width: 100%; height: auto; pointer-events: auto;"
     >
-    {#if index > 0}
+    {#if index === 0}
+        {resetUserInteraction()}
+    {/if}
+
+    {#if index === 1}
+        {setRect()}
+        {resetUserInteraction()}
         {#each edges.edges as e}
                 <polyline
                     points={ // String of coordintates x0,y0 x1,y1
@@ -106,6 +148,8 @@
                     in:draw|global={{intro: true , duration: 1000, delay: 200, easing: cubicInOut }}
                 />
             {/each}
+
+
             {#each nodes.nodes as n}
                 {#if n.id == 0}
                         {setCurrNode(n)}
@@ -122,9 +166,49 @@
                 />
 
             {/each}
-            //rect x="100" y="360" width="50" height="20" fill="green"
             <rect x={$rectX} y={$rectY} width="50" height="20" fill="green" 
             in:fade|global={{intro: true , duration: 500, delay: 10, easing: cubicInOut }} />
-        {/if}
+            {/if}
+
+    {#if index > 1}
+        {setRect()}
+        {resetUserInteraction()}
+        {#each edges.edges as e}
+                <polyline
+                    points={ // String of coordintates x0,y0 x1,y1
+                    String(x(nodes.nodes[e.source].x)) + "," + String(y(nodes.nodes[e.source].y))
+                            + " " + String(x(nodes.nodes[e.target].x)) + "," + String(y(nodes.nodes[e.target].y))
+                            }
+                    stroke={e.color}
+                    stroke-width="3"
+                    
+                />
+            {/each}
+
+
+            {#each nodes.nodes as n}
+                {#if n.id == 0}
+                        {setCurrNode(n)}
+                    {/if}
+                <circle 
+                    key={n.id} 
+                    cx={x(n.x)} 
+                    cy={y(n.y)} 
+                    r="20" 
+                    fill={n.color}
+                />
+
+            {/each}
+
+            {/if}
+
+        
     </svg>
+    <div class="text-box">
+        {#if index >= 0}
+            <p>hello</p>
+        {/if}
+    </div>
+
+    
 </div>
