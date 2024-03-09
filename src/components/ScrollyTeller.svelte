b5ca4fb80dd14a1037b0969a559c9afda048080a
PenelopeKing <pby.king@gmail.com>
2024-03-08 17:53:09 -0800

    make website look better

--- src/components/ScrollyTeller.svelte
+++ src/components/ScrollyTeller.svelte
@@ -20,7 +20,6 @@
   bind:index
   bind:offset
   bind:progress
-
 >
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -35,6 +34,7 @@
       <span id = "nodes-graph" style="margin-left: 20px;"> <Nodes {index} {width} {height} /> </span>
   </div>
 </div>
+
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="foreground" 
@@ -42,88 +42,126 @@ slot="foreground"
 style="pointer-events: none;">
   <section class = "first-page">
     <div class = "header-background">
+      <br/>
+      <br/>
+      <br/>
+      <div class = "block">
         <h1 align="center">An Introduction to Dijkstra Algorithm</h1>
+        <p class = "names" align= "left" style="color:#fcfcfc" >Penny King & Garvey Li</p>
+      </div>
     </div>
-      <p style="color:#fcfcfc" >Penny King & Garvey Li</p>
-      <br/>
-      <br/>
-      <p class = "text" align= left >Welcome to your first day at your job at OOperEats. It is time to make your first delivery. Unfortunately our map system is down so it is up to you to decide the path you want to take to your customer’s destination. Keep in mind that we want to be as quick as possible so you can be ready for your next order pick up!
-        </p>
-      <p class = "text" align= left> Your Goal: Find the fastest path to your destination from the restaurant pick up.</p>
+    <br/>
+    <br/>
+    <br/>
+    <div class = "textbox2">
+      <p class = "text2" align= left >Welcome to your first day at your job at OOperEats. It is time to make your first delivery. Unfortunately our map system is down so it is up to you to decide the path you want to take to your customer’s destination. Keep in mind that we want to be as quick as possible so you can be ready for your next order pick up!</p>
+      <p class = "text2" align= left> Your Goal: Find the fastest path to your destination from the restaurant pick up.</p>
+    </div>
   </section>
   <section>
     <h3 class = "page-head" align = left>Pg 2: Interactive Map</h3>
     <br/>
+    <div class = "textbox">
     <p class = "text" align= left>The restaurant you are currently at is the blue dot at the left.</p>
     <p class = "text" align= left>These pink dots are neighborhoods you can pass through on your drive.</p>
     <p class = "text" align= left>The numbers represent how long it takes to get from one neighborhood to another.</p>
     <p class = "text" align= left>This red dot is your final destination.</p>
     <br />
     <p class = "text" align= left>Try to find the fastest path possible to your destination. Create the path you think is best by clicking on the nodes. Scroll down when you are done!</p>
+  </div>
   </section>
   <section>
     <h3 class = "page-head" align = left>Pg 3: Answer</h3>
     <br/>
+    <div class = "textbox">
     <p class = "text" align= left>Was this what you got?</p>
     <p class = "text" align = left>Finding the shortest path is not a simple task…
       How can we find the concrete shortest path to your customer?
       </p>
     <p class = "text" align = left>One method to do so in this situation is called Dijkstra Algorithm.</p>
-
+  </div>
   </section>
   <section>
     <h3 class = "page-head" align = left>Pg 4: Start of Dijkstra Explanation... Set Up 2</h3>
     <br/>
+    <div class = "textbox">
     <p class = "text" align = left>The Dijkstra Algorithm is very similar to the Breadth First Search algorithm, and lets us find the shortest path between a source and target node on a positively weighted graph (directed or undirected).</p>
       <p class = "text" align = left>To start out, we define a value u.est for each node denoting the distance from the source node. For the initial source node, this equals 0, and is unknown for all other nodes, so we can set them to positive infinity.</p>
-
+    </div>
   </section>
   <section>
     <h3 class = "page-head" align = left>Pg 5: Initialize C and Outside Sets </h3>
     <br/>
+
+    <div class = "textbox">
     <p class = "text" align = left>
-      To help us keep track of which edges we need to update, we’ll categorize the nodes into two sets: C and Outside. Nodes in C have been 'visited', while Nodes in Outside have not.
+      To help us keep track of which edges we need to update, we'll categorize the nodes into two sets: C and Outside. Nodes in C have been 'visited', while Nodes in Outside have not.
     </p>
     <p class = "text" align = left>
-      With the setup done, we’re ready to start.
+      With the setup done, we're ready to start.
     </p>
+  </div>
+
   </section>
   <section>
     <h3 class = "page-head" align = left>Pg 6: Find the closest nodes to C.</h3>
     <br/>
+    <div class = "textbox">
     <p class = "text" align = left>The first step is to look at all the nodes in Outside that are 1 edge away from (or adjacent) the nodes in C. In this case, those nodes are f, b, and c.</p>
+    </div>
+  
   </section>
   <section>
     <h3 class = "page-head" align = left>Pg 7: Calculate the distance between the source node of C and the closest Outside nodes. Update u.est.</h3>
     <br/>
+
+    <div class = "textbox">
     <p class = "text" align = left>Then, we calculate new distance values for each of the nodes adjacent to C.</p>
     <p class = "text" align = left>We do this by adding the edge weight to the u.est value of the respective source node. 
       If the new distance value is less than the respective node’s u.est, then we update the target node’s u.est to be the distance value. For node f, 7 &lt; inf, so f’s new u.est is 7.
     </p>
+    </div>
+
   </section>
   <section>
     <h3 class = "page-head" align = left >Pg 8: Find the unvisited Node that is closest to the source.</h3>
+    
+    <div class = "textbox">
     <p class = "text" align = left>Now, the next step is to find the node in Outside closest to our source node 'a', which in this case is node 'f'.</p> 
+    </div>
   </section>
   <section>
     <h3 class = "page-head" align = left>Pg 9: Move the node from Outside to C</h3>
     <br/>
+
+    <div class = "textbox">
     <p class = "text" align = left></p>
+    </div>
   </section>
   <section>
     <h3 class = "page-head" align = left>Pg 10: Move the node from Outside to C</h3>
     <br/>
+
+    <div class = "textbox">
     <p class = "text" align = left>And now we repeat those steps until there are no more edges to update uest with for any node.</p>
+    </div>
+
   </section>
   <section>
     <h3 class = "page-head" align = left >Pg 11: Explanation cont... </h3>
     <br/>
+
+    <div class = "textbox">
     <p class = "text" align = left>Filler Text</p>
+    </div>
   </section>
   <section>
     <h3 class = "page-head" align = left >Pg 12: Final Solution</h3>
     <br/>
+
+    <div class = "textbox">
     <p class = "text" align = left>Filler Text</p>
+    </div>
   </section>
 </div>
 
@@ -131,16 +169,41 @@ style="pointer-events: none;">
 
 
 <style>
+  .names {
+    margin-left:25%;
+    margin-top:-2%;
+    font-size:0.9em;
+  }
   .page-head {
-    margin-left:71%;
+    margin-left:70%;
     margin-right: 2%;
   }
  .text {
-  margin-left:72%;
-  margin-right: 2%;
+  padding:15px;
+ }
+ .text2 {
+
+  margin: 2%;
+  padding:10px;
+  padding-top: -5px;
+  padding-bottom: - 5px;
+  display: inline-block;
  }
   * {
     font-family: 'Nunito', sans-serif;
+    box-sizing: border-box;
+  }
+  .textbox2 {
+    background: white;
+    margin-left:55%;
+    margin-right: 10%;
+    border-radius:5px;
+  }
+  .textbox {
+    background: white;
+    margin-left:70%;
+    margin-right: 2%;
+    border-radius:5px;
   }
 
   .first-page{
@@ -175,6 +238,7 @@ style="pointer-events: none;">
     height: auto;
     position: relative;
     z-index: 0;
+    background-color: #ede9dd;
   }
   section {
     height: 95vh;
@@ -187,11 +251,18 @@ style="pointer-events: none;">
     margin-top:10px;
   }
 
-  h1 {
+  .block {
     background-color: #499e97;
-    height: 80vh;
-    padding: 60px;
-    margin: 0px;
+    height: 33vh;
+    padding: 40px;
+    margin: 10px;
+    margin-left:10%;
+    margin-top:6.5%;
+    width:80vh;
+    border-radius: 5px;
+  }
+  h1 {
+    margin-top:-1.5%;
   }
 
   h3 {
@@ -203,6 +274,7 @@ style="pointer-events: none;">
     color:white;
     border-radius: 5px;
   }
+
 
 
 </style>
